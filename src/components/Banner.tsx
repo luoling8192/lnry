fd8cd4d5c982e37ed2df40cfdd35ad5d68cd3e68
luoling8192 <rbxin2003@outlook.com>
2021-07-12 20:57:54 +0800

    暂时移除Banner移动端适配
    Signed-off-by: luoling8192 <rbxin2003@outlook.com>

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,3 @@
-import { getBanner } from '@/services/api';
 import styles from '@/styles/layout.less';
 import { Carousel } from 'antd';
 import React from 'react';
@@ -6,33 +5,12 @@ import React from 'react';
 export default class Banner extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
-
     this.state = {
       banner_url: [
         'https://lc-gluttony.s3.amazonaws.com/GaPY9YzdWc2m/7dFypF8bugbr7u8qNwwbtejb5bYaPSGX/banner2.jpg',
       ],
-      screen_width: window.innerWidth,
-      breakpoint: 1024,
     };
   }
-
-  async componentDidMount() {
-    window.addEventListener('resize', this.handleWindowResize);
-    const data = await getBanner(
-      this.state.screen_width < this.state.breakpoint,
-    );
-    this.setState({ banner_url: data });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleWindowResize);
-  }
-
-  handleWindowResize = () => {
-    this.setState({
-      screen_width: window.innerWidth,
-    });
-  };
 
   render() {
     const banner = this.state.banner_url.map(($key: any) => {
